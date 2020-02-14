fb8900d72e4161f546b6d9b9da1862592edeae99
bjwang964 <a1109003964@163.com>
2020-02-14 19:54:44 +0800

    通过行为级验证，包括算数逻辑运算和访存指令

--- RISC_V.runs/RAM32_synth_1/RAM32.tcl
+++ RISC_V.runs/RAM32_synth_1/RAM32.tcl
@@ -32,7 +32,7 @@ set_property default_lib xil_defaultlib [current_project]
 set_property target_language Verilog [current_project]
 set_property ip_output_repo c:/Users/q1109/OneDrive/RISC_V/RISC_V.cache/ip [current_project]
 set_property ip_cache_permissions {read write} [current_project]
-read_ip -quiet c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32.xci
+read_ip -quiet C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32.xci
 set_property used_in_implementation false [get_files -all c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_ooc.xdc]
 
 # Mark all dcp files as not used in implementation to prevent them from being
@@ -88,32 +88,32 @@ write_checkpoint -force -noxdef RAM32.dcp
 create_report "RAM32_synth_1_synth_report_utilization_0" "report_utilization -file RAM32_utilization_synth.rpt -pb RAM32_utilization_synth.pb"
 
 if { [catch {
-  file copy -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32.dcp c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32.dcp
+  file copy -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32.dcp C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  write_verilog -force -mode synth_stub c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.v
+  write_verilog -force -mode synth_stub C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode synth_stub c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.vhdl
+  write_vhdl -force -mode synth_stub C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_verilog -force -mode funcsim c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.v
+  write_verilog -force -mode funcsim C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  write_vhdl -force -mode funcsim c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.vhdl
+  write_vhdl -force -mode funcsim C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -123,32 +123,32 @@ if { [catch {
 
 
 if { [catch {
-  file copy -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32.dcp c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32.dcp
+  file copy -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32.dcp C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32.dcp
 } _RESULT ] } { 
   send_msg_id runtcl-3 error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
   error "ERROR: Unable to successfully create or copy the sub-design checkpoint file."
 }
 
 if { [catch {
-  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_stub.v c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.v
+  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_stub.v C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a Verilog synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_stub.vhdl c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.vhdl
+  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_stub.vhdl C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create a VHDL synthesis stub for the sub-design. This may lead to errors in top level synthesis of the design. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_sim_netlist.v c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.v
+  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_sim_netlist.v C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.v
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the Verilog functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
 
 if { [catch {
-  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_sim_netlist.vhdl c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.vhdl
+  file rename -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.runs/RAM32_synth_1/RAM32_sim_netlist.vhdl C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_sim_netlist.vhdl
 } _RESULT ] } { 
   puts "CRITICAL WARNING: Unable to successfully create the VHDL functional simulation sub-design file. Post-Synthesis Functional Simulation with this file may not be possible or may give incorrect results. Error reported: $_RESULT"
 }
@@ -157,13 +157,13 @@ if { [catch {
 
 if {[file isdir C:/Users/q1109/OneDrive/RISC_V/RISC_V.ip_user_files/ip/RAM32]} {
   catch { 
-    file copy -force c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.v C:/Users/q1109/OneDrive/RISC_V/RISC_V.ip_user_files/ip/RAM32
+    file copy -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.v C:/Users/q1109/OneDrive/RISC_V/RISC_V.ip_user_files/ip/RAM32
   }
 }
 
 if {[file isdir C:/Users/q1109/OneDrive/RISC_V/RISC_V.ip_user_files/ip/RAM32]} {
   catch { 
-    file copy -force c:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.vhdl C:/Users/q1109/OneDrive/RISC_V/RISC_V.ip_user_files/ip/RAM32
+    file copy -force C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/RAM32/RAM32_stub.vhdl C:/Users/q1109/OneDrive/RISC_V/RISC_V.ip_user_files/ip/RAM32
   }
 }
 file delete __synthesis_is_running__
--- RISC_V.srcs/sources_1/ip/ROM_8/ROM_8_sim_netlist.vhdl
+++ RISC_V.srcs/sources_1/ip/ROM_8/ROM_8_sim_netlist.vhdl
@@ -1,7 +1,7 @@
 -- Copyright 1986-2019 Xilinx, Inc. All Rights Reserved.
 -- --------------------------------------------------------------------------------
 -- Tool Version: Vivado v.2019.2 (win64) Build 2708876 Wed Nov  6 21:40:23 MST 2019
--- Date        : Wed Feb 12 20:37:23 2020
+-- Date        : Fri Feb 14 17:51:36 2020
 -- Host        : LAPTOP-AEDG5TQK running 64-bit major release  (build 9200)
 -- Command     : write_vhdl -force -mode funcsim
 --               C:/Users/q1109/OneDrive/RISC_V/RISC_V.srcs/sources_1/ip/ROM_8/ROM_8_sim_netlist.vhdl
@@ -16,7 +16,7 @@ library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
 entity ROM_8_rom is
   port (
-    spo : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    spo : out STD_LOGIC_VECTOR ( 20 downto 0 );
     a : in STD_LOGIC_VECTOR ( 7 downto 0 )
   );
   attribute ORIG_REF_NAME : string;
@@ -24,52 +24,292 @@ entity ROM_8_rom is
 end ROM_8_rom;
 
 architecture STRUCTURE of ROM_8_rom is
-  signal \spo[0]_INST_0_i_1_n_0\ : STD_LOGIC;
+  signal \spo[25]_INST_0_i_1_n_0\ : STD_LOGIC;
   attribute SOFT_HLUTNM : string;
-  attribute SOFT_HLUTNM of \spo[8]_INST_0\ : label is "soft_lutpair0";
-  attribute SOFT_HLUTNM of \spo[9]_INST_0\ : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of \spo[0]_INST_0\ : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of \spo[11]_INST_0\ : label is "soft_lutpair2";
+  attribute SOFT_HLUTNM of \spo[19]_INST_0\ : label is "soft_lutpair1";
+  attribute SOFT_HLUTNM of \spo[25]_INST_0_i_1\ : label is "soft_lutpair2";
+  attribute SOFT_HLUTNM of \spo[4]_INST_0\ : label is "soft_lutpair0";
+  attribute SOFT_HLUTNM of \spo[9]_INST_0\ : label is "soft_lutpair1";
 begin
-\spo[0]_INST_0\: unisim.vcomponents.LUT2
+\spo[0]_INST_0\: unisim.vcomponents.LUT5
     generic map(
-      INIT => X"2"
+      INIT => X"1FFF0000"
     )
         port map (
-      I0 => \spo[0]_INST_0_i_1_n_0\,
-      I1 => a(7),
+      I0 => a(1),
+      I1 => a(2),
+      I2 => a(4),
+      I3 => a(3),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
       O => spo(2)
     );
-\spo[0]_INST_0_i_1\: unisim.vcomponents.LUT6
+\spo[10]_INST_0\: unisim.vcomponents.LUT6
     generic map(
-      INIT => X"0000000000000001"
+      INIT => X"1F5F000000000000"
     )
         port map (
-      I0 => a(5),
-      I1 => a(3),
-      I2 => a(1),
-      I3 => a(2),
-      I4 => a(4),
-      I5 => a(6),
-      O => \spo[0]_INST_0_i_1_n_0\
+      I0 => a(3),
+      I1 => a(1),
+      I2 => a(4),
+      I3 => a(0),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(2),
+      O => spo(5)
     );
-\spo[8]_INST_0\: unisim.vcomponents.LUT3
+\spo[11]_INST_0\: unisim.vcomponents.LUT5
     generic map(
-      INIT => X"04"
+      INIT => X"00010000"
+    )
+        port map (
+      I0 => a(4),
+      I1 => a(7),
+      I2 => a(6),
+      I3 => a(5),
+      I4 => a(3),
+      O => spo(6)
+    );
+\spo[12]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"1D41000003780000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(0),
+      I2 => a(4),
+      I3 => a(2),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(1),
+      O => spo(7)
+    );
+\spo[13]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"2207000030A20000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(4),
+      I2 => a(0),
+      I3 => a(1),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(2),
+      O => spo(8)
+    );
+\spo[14]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"3770000000620000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(4),
+      I2 => a(0),
+      I3 => a(1),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(2),
+      O => spo(9)
+    );
+\spo[16]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"57FF000000000000"
+    )
+        port map (
+      I0 => a(4),
+      I1 => a(2),
+      I2 => a(1),
+      I3 => a(3),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(0),
+      O => spo(10)
+    );
+\spo[17]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"0000000700000000"
+    )
+        port map (
+      I0 => a(4),
+      I1 => a(3),
+      I2 => a(5),
+      I3 => a(6),
+      I4 => a(7),
+      I5 => a(1),
+      O => spo(11)
+    );
+\spo[18]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"0000000700000000"
+    )
+        port map (
+      I0 => a(4),
+      I1 => a(3),
+      I2 => a(5),
+      I3 => a(6),
+      I4 => a(7),
+      I5 => a(2),
+      O => spo(12)
+    );
+\spo[19]_INST_0\: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"222A0000"
+    )
+        port map (
+      I0 => \spo[25]_INST_0_i_1_n_0\,
+      I1 => a(4),
+      I2 => a(2),
+      I3 => a(1),
+      I4 => a(3),
+      O => spo(13)
+    );
+\spo[20]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"302FFFFF00000000"
     )
         port map (
       I0 => a(0),
-      I1 => \spo[0]_INST_0_i_1_n_0\,
-      I2 => a(7),
+      I1 => a(4),
+      I2 => a(2),
+      I3 => a(1),
+      I4 => a(3),
+      I5 => \spo[25]_INST_0_i_1_n_0\,
+      O => spo(14)
+    );
+\spo[21]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"40000D004E006C00"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(0),
+      I2 => a(2),
+      I3 => \spo[25]_INST_0_i_1_n_0\,
+      I4 => a(1),
+      I5 => a(4),
+      O => spo(15)
+    );
+\spo[22]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"333F000030400000"
+    )
+        port map (
+      I0 => a(0),
+      I1 => a(3),
+      I2 => a(2),
+      I3 => a(4),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(1),
+      O => spo(16)
+    );
+\spo[23]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"4000000057880000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(1),
+      I2 => a(0),
+      I3 => a(2),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(4),
+      O => spo(17)
+    );
+\spo[24]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"0222022A00000000"
+    )
+        port map (
+      I0 => \spo[25]_INST_0_i_1_n_0\,
+      I1 => a(2),
+      I2 => a(1),
+      I3 => a(4),
+      I4 => a(0),
+      I5 => a(3),
+      O => spo(18)
+    );
+\spo[25]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"1000104000000000"
+    )
+        port map (
+      I0 => a(4),
+      I1 => a(2),
+      I2 => \spo[25]_INST_0_i_1_n_0\,
+      I3 => a(1),
+      I4 => a(0),
+      I5 => a(3),
+      O => spo(20)
+    );
+\spo[25]_INST_0_i_1\: unisim.vcomponents.LUT3
+    generic map(
+      INIT => X"01"
+    )
+        port map (
+      I0 => a(7),
+      I1 => a(6),
+      I2 => a(5),
+      O => \spo[25]_INST_0_i_1_n_0\
+    );
+\spo[30]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"1200010002002000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(4),
+      I2 => a(2),
+      I3 => \spo[25]_INST_0_i_1_n_0\,
+      I4 => a(1),
+      I5 => a(0),
+      O => spo(19)
+    );
+\spo[4]_INST_0\: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"01FF0000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => a(1),
+      I2 => a(2),
+      I3 => a(4),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
       O => spo(0)
     );
-\spo[9]_INST_0\: unisim.vcomponents.LUT3
+\spo[5]_INST_0\: unisim.vcomponents.LUT6
     generic map(
-      INIT => X"08"
+      INIT => X"4202000057570000"
     )
         port map (
-      I0 => a(0),
-      I1 => \spo[0]_INST_0_i_1_n_0\,
-      I2 => a(7),
+      I0 => a(3),
+      I1 => a(1),
+      I2 => a(2),
+      I3 => a(0),
+      I4 => \spo[25]_INST_0_i_1_n_0\,
+      I5 => a(4),
       O => spo(1)
+    );
+\spo[8]_INST_0\: unisim.vcomponents.LUT6
+    generic map(
+      INIT => X"0C4C4C4C00000000"
+    )
+        port map (
+      I0 => a(3),
+      I1 => \spo[25]_INST_0_i_1_n_0\,
+      I2 => a(4),
+      I3 => a(2),
+      I4 => a(1),
+      I5 => a(0),
+      O => spo(3)
+    );
+\spo[9]_INST_0\: unisim.vcomponents.LUT5
+    generic map(
+      INIT => X"3F004000"
+    )
+        port map (
+      I0 => a(2),
+      I1 => a(4),
+      I2 => a(3),
+      I3 => \spo[25]_INST_0_i_1_n_0\,
+      I4 => a(1),
+      O => spo(4)
     );
 end STRUCTURE;
 library IEEE;
@@ -78,7 +318,7 @@ library UNISIM;
 use UNISIM.VCOMPONENTS.ALL;
 entity ROM_8_dist_mem_gen_v8_0_13_synth is
   port (
-    spo : out STD_LOGIC_VECTOR ( 2 downto 0 );
+    spo : out STD_LOGIC_VECTOR ( 20 downto 0 );
     a : in STD_LOGIC_VECTOR ( 7 downto 0 )
   );
   attribute ORIG_REF_NAME : string;
@@ -90,7 +330,7 @@ begin
 \gen_rom.rom_inst\: entity work.ROM_8_rom
      port map (
       a(7 downto 0) => a(7 downto 0),
-      spo(2 downto 0) => spo(2 downto 0)
+      spo(20 downto 0) => spo(20 downto 0)
     );
 end STRUCTURE;
 library IEEE;
@@ -187,7 +427,7 @@ end ROM_8_dist_mem_gen_v8_0_13;
 
 architecture STRUCTURE of ROM_8_dist_mem_gen_v8_0_13 is
   signal \<const0>\ : STD_LOGIC;
-  signal \^spo\ : STD_LOGIC_VECTOR ( 9 downto 0 );
+  signal \^spo\ : STD_LOGIC_VECTOR ( 30 downto 0 );
 begin
   dpo(31) <= \<const0>\;
   dpo(30) <= \<const0>\;
@@ -285,32 +525,18 @@ begin
   qspo(2) <= \<const0>\;
   qspo(1) <= \<const0>\;
   qspo(0) <= \<const0>\;
-  spo(31) <= \<const0>\;
-  spo(30) <= \<const0>\;
-  spo(29) <= \<const0>\;
-  spo(28) <= \<const0>\;
-  spo(27) <= \<const0>\;
-  spo(26) <= \<const0>\;
-  spo(25) <= \<const0>\;
-  spo(24) <= \^spo\(0);
-  spo(23) <= \<const0>\;
-  spo(22) <= \^spo\(0);
-  spo(21) <= \<const0>\;
-  spo(20) <= \<const0>\;
-  spo(19) <= \<const0>\;
-  spo(18) <= \^spo\(0);
-  spo(17 downto 16) <= \^spo\(9 downto 8);
-  spo(15) <= \^spo\(0);
-  spo(14) <= \<const0>\;
-  spo(13) <= \<const0>\;
-  spo(12) <= \^spo\(9);
-  spo(11) <= \<const0>\;
-  spo(10) <= \^spo\(0);
-  spo(9 downto 8) <= \^spo\(9 downto 8);
-  spo(7) <= \<const0>\;
+  spo(31) <= \^spo\(25);
+  spo(30) <= \^spo\(30);
+  spo(29) <= \^spo\(25);
+  spo(28) <= \^spo\(25);
+  spo(27) <= \^spo\(25);
+  spo(26) <= \^spo\(25);
+  spo(25 downto 16) <= \^spo\(25 downto 16);
+  spo(15) <= \<const0>\;
+  spo(14 downto 8) <= \^spo\(14 downto 8);
+  spo(7) <= \^spo\(0);
   spo(6) <= \<const0>\;
-  spo(5) <= \<const0>\;
-  spo(4) <= \<const0>\;
+  spo(5 downto 4) <= \^spo\(5 downto 4);
   spo(3) <= \<const0>\;
   spo(2) <= \<const0>\;
   spo(1) <= \^spo\(0);
@@ -322,8 +548,12 @@ GND: unisim.vcomponents.GND
 \synth_options.dist_mem_inst\: entity work.ROM_8_dist_mem_gen_v8_0_13_synth
      port map (
       a(7 downto 0) => a(7 downto 0),
+      spo(20) => \^spo\(25),
+      spo(19) => \^spo\(30),
+      spo(18 downto 10) => \^spo\(24 downto 16),
+      spo(9 downto 3) => \^spo\(14 downto 8),
       spo(2) => \^spo\(0),
-      spo(1 downto 0) => \^spo\(9 downto 8)
+      spo(1 downto 0) => \^spo\(5 downto 4)
     );
 end STRUCTURE;
 library IEEE;
